12136c848d8716dd4491baeb187147e80b917180
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-05-27 21:34:46 -0300

    Montei pag de conclusão de cadastro

--- src/components/ConclusaoDoCadastro.vue
+++ src/components/ConclusaoDoCadastro.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="container-general">
-    <h1>Conclusão de cadastro</h1>
+    <div class="container-main">
+      <header>
+        <h1>Cadastro finalizado!</h1>
+      </header>
+      <main>
+        <img
+          src="../assets/design-pag-4.png"
+          alt="logo"
+        >
+        <section>
+          <h2>Vamos inovar juntos?</h2>
+          <p style="text-align: center; margin-top: 10px">
+            Conquiste novos clientes, preencha sua agenda e elimine faltas, seja o melhor médico que você pode ser.
+          </p>
+        </section>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -24,5 +40,51 @@ export default {
   display: flex;
   justify-content: center;
   flex-flow: column nowrap;
+}
+
+.container-main {
+  background-color: white;
+  align-items: center;
+  height: 90%;
+  margin: auto;
+  margin-bottom: 0;
+  width: 100%;
+  display: flex;
+  border-radius: 30px 30px 0 0;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  padding: 20px;
+	padding-top: 20px;
+  padding-bottom: 0px;
+}
+
+.container-main h1 {
+	color: #3f2796;
+  margin-top: 40px;
+	margin-bottom: 40px;
+}
+
+main {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+main img {
+  margin: auto;
+}
+
+section {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+section h2 {
+  text-align: center;
 }
 </style>
